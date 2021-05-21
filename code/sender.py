2e4599420e823f0d1d893dd44350a9d96855f3ed
Martin <martin.stecher@gmx.at>
2021-05-21 16:57:27 +0200

    16.49

--- code/sender.py
+++ code/sender.py
@@ -128,8 +128,7 @@ class GBNSender(Automaton):
                 header_GBN = GBN(type = 0, hlen = 48, num = self.current, win = self.win)
                 send(IP(src=self.sender, dst=self.receiver)/header_GBN/self.buffer[self.current])
 
-
-                print("hello")
+                log.debug("hello %s", self.win)
 
 
                 ###############################################################
