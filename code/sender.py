11fb2197d754274b65cc429d15fdceccbbd62640
Mathis Lindner <mathis.lindner@gmail.com>
2021-05-24 16:05:13 +0200

    pop

--- code/sender.py
+++ code/sender.py
@@ -175,7 +175,7 @@ class GBNSender(Automaton):
             ################################################################
 
             while self.unack != ack:
-                del self.buffer[str(self.unack)]
+                self.buffer.pop(self.unack)
                 if ack == 0:
                     ack = 2**self.n_bits -1
                 else:
