88b889118ce73dccb4905c8d769f068df6f6c4fc
mlindner <mathis.lindner@gmail.com>
2021-06-02 17:50:30 +0200

    self.end disabled

--- code/receiver_sven.py
+++ code/receiver_sven.py
@@ -318,7 +318,7 @@ class GBNReceiver(Automaton):
 
                 # last packet received and all ACKs successfully transmitted
                 # --> close receiver
-                if self.end_receiver and self.end_num == self.next:
+                if self.end_receiver and self.end_num == self.next and False:
                     raise self.END()
 
             # transition to WAIT_SEGMENT to receive next segment
