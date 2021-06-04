279ff59c42565f306a6be711548c94d232edc3f6
sgutjahr <sgutjahr@student.ethz.ch>
2021-06-04 13:57:26 +0000

    Update sender_Sven_Q1_Q2.py

--- code/sender_Sven_Q1_Q2.py
+++ code/sender_Sven_Q1_Q2.py
@@ -184,7 +184,7 @@ class GBNSender(Automaton):
                         send(IP(src=self.sender, dst=self.receiver) / header_GBN / self.buffer[ack])
                         # add to self.buffer bc we just resent?
                         self.acks_received[ack] = 0
-                else:
+                elif ack <= self.win + self.unack:
                     self.acks_received[ack] = 1
 
             while self.unack != ack:
